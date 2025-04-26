4bb7f1ebdc0767a50fa94949984f5280e8794b7e
Flávio <74250863+Wiiito@users.noreply.github.com>
2025-04-25 21:43:39 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/github-profile-search",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
